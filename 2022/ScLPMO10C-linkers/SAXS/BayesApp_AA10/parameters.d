9dd2a0ef29cdd3d7c5a661653febaf6259e72212
gcourtade <47104451+gcourtade@users.noreply.github.com>
2023-03-06 15:03:50 +0100

    Updated SAXS

---
+++ 2022/ScLPMO10C-linkers/SAXS/BayesApp_AA10/parameters.d
@@ -0,0 +1,25 @@
+ Input file name  =  regrid_001_AA10.dat                             
+ qmin and qmax    =    0.0080   0.7217
+ Number of data points               =           81
+ Number of points in p(r)            =           84
+ Number of error calculations        =          493
+ Errors are too low when few error calc. are used
+
+ Convergence of algorithm almost OK...
+
+ I(0) estimated             : 0.154E-01  +- .179E-05 
+ Maximum diameter           :     45.96  +-     0.79 
+ Radius of gyration         :     16.10  +-     0.00 
+ Axial ratio from p(r) (pro):      1.36  +-     0.00 
+ Axial ratio from p(r) (obl):      0.70  +-     0.00 
+ Reduced Chi-square         :      1.22  +-     0.01 
+ Background estimated       :                0.00011
+ Log(alpha) (smoothness)    :     14.34  +-     0.16 
+ Number of good parameters  :      8.41  +-     0.14 
+ Number of Shannon channels :     10.44
+ Evidence at maximum        : -0.78E+02  +-  0.37E+00 
+ Probability of chi-square  :  0.25E+00
+ The exp errors are probably: Correct
+ Correction factor          :      1.10
+
+ CPU time used:   19.4 seconds of maximum  900.0 seconds
---
+++ 2022/ScLPMO10C-linkers/SAXS/BayesApp_full-lengthWT/parameters.d
@@ -0,0 +1,25 @@
+ Input file name  =  regrid_001.dat                                  
+ qmin and qmax    =    0.0085   0.4939
+ Number of data points               =           86
+ Number of points in p(r)            =           84
+ Number of error calculations        =          494
+ Errors are too low when few error calc. are used
+
+ Convergence of algorithm OK
+
+ I(0) estimated             : 0.307E-01  +- .177E-04 
+ Maximum diameter           :    151.17  +-     1.11 
+ Radius of gyration         :     38.89  +-     0.05 
+ Axial ratio from p(r) (pro):      6.75  +-     0.06 
+ Axial ratio from p(r) (obl):      0.10  +-     0.00 
+ Reduced Chi-square         :      0.86  +-     0.01 
+ Background estimated       :                0.00023
+ Log(alpha) (smoothness)    :     12.83  +-     0.20 
+ Number of good parameters  :     19.16  +-     0.49 
+ Number of Shannon channels :     23.35
+ Evidence at maximum        : -0.94E+02  +-  0.48E+00 
+ Probability of chi-square  :  0.43E+00
+ The exp errors are probably: Correct
+ Correction factor          :      0.93
+
+ CPU time used:   19.6 seconds of maximum  900.0 seconds
