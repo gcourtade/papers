047180245b500d066f7b26d4bd04f329c9cb184b
Gaston Courtade <gaston.courtade@ntnu.no>
2025-10-17 11:30:35 +0200

    updated CSI analysis

--- 2025/AA14-peptides/NMR_P3/RCI_analysis/CSI3_analysis.py
+++ 2025/AA14-peptides/NMR_P3/RCI_analysis/CSI3_analysis.py
@@ -22,17 +22,22 @@ resnr = csi3[:,0].astype(int)
 rci = np.where(csi3[:,4] == 'NA','0.0', csi3[:,4]).astype(float)
 
 
-# In[17]:
+# In[49]:
 
 
 # A lower RCI value indicates a more rigid region
 # This paper should be additionally cited, e.g. in the figure caption, when using RCI from CSI3.0:
 # https://pubs.acs.org/doi/10.1021/ja054842f
 #
-plt.plot(resnr, rci, 'k-', label='RCI from CSI3')
+plt.plot(resnr, rci, 'k.-', markersize=5, linewidth=1.6)
+plt.plot(resnr[7:14], rci[7:14], '.-', markersize=8, linewidth=2, color='#f1bc3d', label='Helical region')
 plt.ylabel('RCI from CSI3.0')
 plt.xlabel('Residue Number')
 plt.xlim(resnr.min(), resnr.max())
+plt.legend()
+plt.savefig('AA14_peptide_RCI_CSI3.png', dpi=300)
+plt.savefig('AA14_peptide_RCI_CSI3.pdf', dpi=300)
+plt.show()
 
 
 # In[ ]:
